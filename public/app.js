919d2e8022c2a86054dd6a35784a8d79be794783
anikisana <younker12345@gmail.com>
2022-12-26 17:58:07 +0600

    task completion

--- public/app.js
+++ public/app.js
@@ -40,6 +40,22 @@ new Vue({
       },
       removeTodo(id) {
         this.todos = this.todos.filter(t => t.id !== id)
+      },
+      completeTodo(id) {
+        console.log(id);
+        fetch('/api/todo/' + id, {
+          method: 'put',
+          headers: {'Content-Type' : 'application/json'},
+          body: JSON.stringify({done:true})
+        })
+        .then(res => res.json())
+        .then(({todo}) => {
+          const ind = this.todos.findIndex(t => t.id === todo.id);
+          this.todos[ind].updatedAt = todo.updatedAt;
+        })
+        .catch(e => {
+          console.log(e);
+        })
       }
     },
     filters: {
@@ -57,7 +73,7 @@ new Vue({
           options.minute = '2-digit';
           options.second = '2-digit';
         }
-        return new Intl.DateTimeFormat('ru-RU',   ).format(new Date(value))
+        return new Intl.DateTimeFormat('ru-RU', options).format(new Date(value))
       }
     }
   })
